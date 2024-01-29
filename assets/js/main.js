0ede0ba33022f188931ca456de27cadb66ef11a5
PaulAjii <paulajii134@gmail.com>
2024-01-29 18:24:31 +0100

    Finished Mobile View

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+const hamburger = document.querySelector('.bx-menu')
+const closeNav = document.querySelector('.close__btn')
+const navMenu = document.querySelector('.nav__menu')
+const navLinks = document.querySelectorAll('.nav__link')
+const copyDate = document.querySelector('.date')
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.add('nav__open')
+})
+
+closeNav.addEventListener('click', () => {
+  navMenu.classList.remove('nav__open')
+})
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', () => navMenu.classList.remove('nav__open'))
+})
+
+copyDate.innerText = new Date().getFullYear()
