eae441b04bf3a146aa1e8794bfec865ffa63a756
PaulAjii <paulajii134@gmail.com>
2024-01-31 08:24:17 +0100

    Finished Desktop View

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,11 @@
 const hamburger = document.querySelector('.bx-menu')
-const closeNav = document.querySelector('.close__btn')
+// const closeNav = document.querySelector('.close__btn')
 const navMenu = document.querySelector('.nav__menu')
 const navLinks = document.querySelectorAll('.nav__link')
 const copyDate = document.querySelector('.date')
 
 hamburger.addEventListener('click', () => {
-  navMenu.classList.add('nav__open')
-})
-
-closeNav.addEventListener('click', () => {
-  navMenu.classList.remove('nav__open')
+  navMenu.classList.toggle('nav__open')
 })
 
 navLinks.forEach((navLink) => {
